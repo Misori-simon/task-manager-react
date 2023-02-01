31adac2fd36e603e0296243259a9c89eb9daa67f
Misori Simon <misorisimon@gmail.com>
2023-02-01 09:53:20 +0100

    -> Added Project component to return Project and Projects, -> Added projectLayout function

---
+++ src/components/Projects.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Tasks from './Tasks'
+
+function projectLayout (project) {
+    return (
+        <div className='project-item'>
+            <div className='project-preview'>
+                <div className='project-icon'><img src="" alt="" /></div>
+                <h3 className='project-title'>{project.title}</h3>
+                <div className='project-count'>
+                    <p className='project-tasks-count'>{project.tasks.length}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Project(project) {
+    return (
+        <div className='project-wrap'>
+            <div className='project-info'>
+                <div className='left'>
+                    <h3 className='project-title'>{project.title}</h3>
+                    <h3 className='project-month'>December 2020</h3>
+                </div>
+                <div className='new-task-btn'>
+                    <button>new task</button>
+                </div>
+            </div>
+            <Tasks taskArr={project.tasks}/>
+        </div>
+    )
+}
+
+function Projects(projectArr) {
+    return (
+        <div className='project-group'>
+            {projectArr.map(project => projectLayout(project))}
+        </div>
+    )
+}
+
+
+
+export {Projects, Project}
